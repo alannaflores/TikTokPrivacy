5db3f2680d3e784eeb971dd7f625134bbd6bc217
alannaflores <alannaflores613>
2022-03-03 15:50:58 -0800

    Begin repo

---
+++ README.md
@@ -0,0 +1,82 @@
+# Developer environment setup
+A couple of notes before getting started:
+  * The following setup instructions were tested on MacOS.
+  * Any line prefixed by `$` is intended to be run in the terminal, but don't include the `$` in the command), lines prefixed with `#` are comments.
+
+Setup instructions:
+
+0. Install git and clone the repo:
+  * Follow the instructions [here](https://github.com/git-guides/install-git) to install git on your local machine.
+  * Follow the instructions [here](https://docs.github.com/en/get-started/getting-started-with-git) to set up your github credentials.
+  * Clone the [repo](https://github.com/TikTokPrivacy).
+
+1. Make sure you have Python 3.8.6 or higher installed.
+  * In a terminal run `python3 --version` or `python3.10 --version` if either of these displays `Python 3.10.2` then you are good to go.
+  * If you don't have the correct version of Python installed, you can download and install it from [here](https://www.python.org/downloads/macos/) (make sure to select version 3.10.2).
+  * After installing Python open a new terminal window and try `python3.8 --version`
+  * Make sure to use the above `python3.8` command when you run the following steps.
+2. Create a virtual environment and install the required dependencies. Run the following commands in a terminal window:
+```
+# Setup virtual env
+$ python3.8 -m venv /path/to/new/virtual/environment
+# Activate virtual env
+$ source /path/to/new/virtual/environment/bin/activate
+# Install npm (assuming you have homebrew installed)
+$ brew install node 
+# Install tiktok scraper 
+$ npm i -g tiktok-scraper
+# Install dependencies
+$ pip install -r /path/to/github/requirements.txt
+```
+
+3. Grabbing videos from tiktok (make sure that you always run all commands below in a terminal window with the virtual env active):
+```
+$ cd /path/to/github/TikTokPrivacy
+$ tiktok-scraper --help
+
+Usage: tiktok-scraper <command> [options]
+
+Commands:
+  tiktok-scraper user [id]     Scrape videos from username. Enter only username
+  tiktok-scraper hashtag [id]  Scrape videos from hashtag. Enter hashtag without #
+  tiktok-scraper trend         Scrape posts from current trends
+  tiktok-scraper music [id]    Scrape posts from a music id number
+  tiktok-scraper video [id]    Download single video without the watermark
+  tiktok-scraper history       View previous download history
+  tiktok-scraper from-file [file] [async]  Scrape users, hashtags, music, videos mentioned
+                                in a file. 1 value per 1 line
+
+Examples:
+  tiktok-scraper user USERNAME -d -n 100 --session sid_tt=dae32131231
+  tiktok-scraper trend -d -n 100 --session sid_tt=dae32131231
+  tiktok-scraper hashtag HASHTAG_NAME -d -n 100 --session sid_tt=dae32131231
+  tiktok-scraper music MUSIC_ID -d -n 50 --session sid_tt=dae32131231
+ 
+Some that are useful for our project (--t allows us to download meta data to csv):
+  tiktok-scraper hashtag adhdtips --t "csv"
+  tiktok-scraper hashtag aboutmechallenge  --download images --n 7  --t "csv"
+
+For more information check out their github: https://github.com/drawrowfly/tiktok-scraper
+```
+
+4. Run `video2data.py` for the videos you have obtained in order to extract a dataframe of information.
+
+# Developer workflow
+
+1) Create a new branch from `main`:
+```
+$ cd path/to/github/TikTokPRivacy
+# make sure you are on the main branch and have the latest version of main
+$ git checkout main
+$ git pull origin main
+# create your new branch
+$ git checkout -b my_cool_feature
+```
+2) Make your changes and test them out locally (see Testing locally above for how to run the app).
+3) Push your changes to github
+```
+$ git push origin my_cool_feature
+```
+4) Open a pull request [here](https://github.com/TikTokPrivacy/pulls). Your branch should show up automatically with a button to create a new pull request, if you don't see it then you can manually create a pull request by clicking the `New pull request` button and setting then choosing your branch name (`my_cool_feature`) from the dropdown for the `compare` button (the `base` branch should always be `main`).
+5) Add a descriptive title and description to your pull request.
+6) Tag the appropriate people to review your pull request.
---
+++ video2data.py
@@ -0,0 +1,130 @@
+"""
+A Four Part Pipeline to Extracting Tiktok User Data
+"""
+import os
+import cv2
+import time
+import geonamescache
+import pandas as pd
+import numpy as np
+import pytesseract as pt
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
+import matplotlib.pyplot as plt
+from imutils.perspective import four_point_transform
+from TikTokApi import TikTokApi
+
+# Note: This works well!
+def convert_video_to_image(VIDEO_PATH, IMAGE_PATH):
+  KPS = 1
+  EXTENSION = ".png"
+  cap = cv2.VideoCapture(VIDEO_PATH)
+  # how frequently a video frame should be converted to image
+  hop = round(cap.get(cv2.CAP_PROP_FPS) / KPS)
+  curr_frame = 0
+
+  while(True):
+    ret, frame = cap.read()
+    if not ret or curr_frame > 400:
+      break
+    if curr_frame % hop == 0:
+      name = IMAGE_PATH + "_" + str(curr_frame) + EXTENSION
+      cv2.imwrite(name, frame)
+    curr_frame += 1
+  cap.release()
+
+
+# Note: This does not do a great job yet of extracting text in images - still debugging this
+def convert_image_to_text(IMAGE_PATH):
+  text = " "
+  # iterating the images inside the folder
+  for imageName in os.listdir(IMAGE_PATH):
+    inputPath = os.path.join(IMAGE_PATH, imageName)
+    image = cv2.imread(inputPath)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([0, 0, 0])
+    upper = np.array([100, 175, 110])
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Morph close to connect individual text into a single contour
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
+
+    # Find rotated bounding box then perspective transform
+    cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    rect = cv2.minAreaRect(cnts[0])
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(image, [box], 0, (36, 255, 12), 2)
+    warped = four_point_transform(255 - mask, box.reshape(4, 2))
+
+    # Perform OCR
+    text += pt.image_to_string(warped, lang='eng', config='--psm 6')
+
+    file1 = open(TEXT_PATH, "a+")
+
+    # providing the name of the image
+    file1.write(imageName + "\n")
+
+    # providing the content in the image
+    file1.write(text + "\n")
+    file1.close()
+
+  file2 = open(TEXT_PATH, 'r')
+  file2.close()
+
+"""
+Once the text is detected we will need to identify which words correspond with 
+I think we need to make a list of all the possibilities for these things:
+-Locations
+-Age 
+-Profession
+-Height 
+-Gender - she first then they then he 
+
+and then check if a word we have extracted from the images exists in these lists
+of possibilities 
+
+I've started below by creating a list of all possible locations
+"""
+def convert_text_to_dataframe():
+  ## TODO: import text file
+  gc = geonamescache.GeonamesCache()
+  countries = list(pd.DataFrame(gc.get_countries()).T["name"])
+  states = list(pd.DataFrame(gc.get_us_states()).T["name"])
+  cities = list(pd.DataFrame(gc.get_cities()).T["name"])
+  locations_list = countries + states + cities
+
+
+def get_unique_users_by_trend(BASIC_PATH, list_of_trends):
+  dfs = []
+  for trend in list_of_trends:
+    tag = api.hashtag(name=trend)
+    users = []
+    # Can't really go above looking at the users for more than 1000
+    # because of limited computational capacity (tiktok API times you out; we would need to spin up proxies)
+    for video in tag.videos(count=5000):
+      users.append(video.author.user_id)
+    dfs.append(users)
+    unique_ids = len(users)
+    print(trend + ": " + str(unique_ids))
+  intersection_ids = list(set.intersection(*map(set, dfs)))
+  total_unique_ids = len(intersection_ids)
+  print("Unique users who have done all trends: " + str(total_unique_ids))
+
+BASIC_PATH = "/Users/alanna/Github/TikTokPrivacy/"
+VIDEO_PATH = 'sample_video.mp4'
+IMAGE_PATH = BASIC_PATH + "images/"
+TEXT_PATH =  BASIC_PATH + "outputFile.txt"
+#convert_video_to_image(VIDEO_PATH, IMAGE_PATH)
+#convert_image_to_text(IMAGE_PATH, TEXT_PATH)
+#convert_text_to_dataframe(TEXT_PATH)
+
+# assumes you have csvs in the csv folder for each trend you want to look at
+list_of_trends = ["adhd", "aboutmechallenge"]
+api = TikTokApi(custom_verify_fp=os.environ.get("verifyFp", None))
+get_unique_users_by_trend(BASIC_PATH, list_of_trends)
+
+
+
+
